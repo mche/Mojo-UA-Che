4d691e6137a9d5d5c3444760423ab5a61a87d1fb
Mikhail Che <ernesto.chege@yandex.ru>
2016-09-09 16:40:06 +0500

    1

--- lib/Mojo/UA/Che.pm
+++ lib/Mojo/UA/Che.pm
@@ -1,6 +1,76 @@
 package Mojo::UA::Che;
 
 use Mojo::Base -base;
+use Mojo::UserAgent;
+
+#text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+has headers => sub { {Accept=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Accept-Language'=>'en-US;q=0.8,en;q=0.6', 'Cache-Control'=>'max-age=0', }};#Connection=>'keep-alive',
+
+my @ua_name = (
+#http://digitorum.ru/blog/2012/12/02/User-Agent-Poiskovye-boty.phtml
+  #~ 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
+'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
+'Googlebot/2.1 (+http://www.google.com/bot.html)',
+  'Googlebot-Video/1.0',
+  'Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)',
+  'Mozilla/5.0 (compatible; YandexMedia/3.0; +http://yandex.com/bots)',
+  'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
+  
+);
+
+has ua_name => sub {$ua_name[rand @ua_name];};
+
+has max_redirects => 3;
+
+has max_try => 3; # цикл попыток
+
+has max_queque => 0; # 0 - неограниченное количество агентов в пуле
+
+has ua_class => 'Mojo::UA::UA';
+
+has debug => 0;
+
+has [qw'proxy_module proxy connect_timeout'];
+
+has proxy_handler => sub {my $self = shift; return unless $self->proxy_module; $self->proxy_module->new};
+
+has proxy_not => sub {[]};
+
+sub ua {
+  my $self = shift;
+  
+  return $self->ua_class->new(ua => $self->_dequeue, top => $self);
+  
+}
+
+
+sub mojo_ua {
+  my $self = shift;
+  my $ua = Mojo::UserAgent->new;
+  # Ignore all cookies
+  $ua->cookie_jar->ignore(sub { 1 });
+  $ua->max_redirects($self->max_redirects);
+  # Change name of user agent
+  $ua->transactor->name($self->ua_name)
+    if $self->ua_name;
+  
+  if ($self->proxy) { $ua->proxy->http($self->proxy)->https($self->proxy); }
+  elsif ($self->proxy_handler) {
+    $ua->proxy_handler($self->proxy_handler);
+    
+  }
+  
+  $ua->proxy->not($self->proxy_not)
+    if $self->proxy_not;
+  
+  $ua->connect_timeout($self->connect_timeout)
+    if $self->connect_timeout;
+  $ua;
+}
+
+
+
 =pod
 
 =encoding utf-8
@@ -13,7 +83,7 @@ use Mojo::Base -base;
 
 =head1 NAME
 
-Mojo::UA::Che - 
+Mojo::UA::Che - Like (idea) Mojo::Pg for mojo user agent
 
 =head1 VERSION
 
@@ -41,9 +111,9 @@ Perhaps a little code snippet.
 
 =head1 SEE ALSO
 
-L<Mojo::Pg>
+L<Mojo::UA>
 
-L<DBI>
+L<Mojo::Pg>
 
 =head1 AUTHOR
 
