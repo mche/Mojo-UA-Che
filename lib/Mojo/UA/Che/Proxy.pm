48b06c910d205b0b08c16b0a98a52ad062a9f5ff
Mikhail Che <ernesto.chege@yandex.ru>
2016-09-10 23:04:44 +0500

    1

---
+++ lib/Mojo/UA/Che/Proxy.pm
@@ -0,0 +1,82 @@
+package Mojo::UA::Che::Proxy;
+use Mojo::Base -base;
+use Mojo::UserAgent;
+
+has ua_has => sub { {} };
+has ua_name => 'SuperAgent 0.07';
+has ua => sub {
+  my $self = shift;
+  my $ua = Mojo::UserAgent->new( 
+    #~ max_redirects=>0,
+    #~ connect_timeout=>30,
+    %{self->ua_has},
+  );
+  $ua->transactor->name();
+  $ua->proxy->not(['hideme.ru'])
+};
+
+has proxy_url => 'http://hideme.ru/proxy-list/?type=45#list';
+has check_url => '';
+
+has list => sub {[]};
+
+
+sub proxy_load {# загрузка списка
+  my $self = shift;
+  my $res = $self->ua->get($self->proxy_url,);
+  die sprintf("Ошибка запроса [%s] списка проксей: %s", $self->proxy_url, $res)
+    unless ref $res;
+  my $sth_new = $self->model->sth('new proxy');
+  my $sth_get = $self->model->sth('get proxy');
+  $res->dom->find('table.proxy__t tbody tr')->map(sub {
+    my $ip = $_->at('td.tdl');
+    my $port = $ip->next_node;
+    my $type = lc((split /,/, $_->child_nodes->[-3]->content)[-1]) ;
+    my $proxy = $ip->content.':'.$port->content;
+    #~ return [$ip->content, $port->content, $type];
+    push @{$self->list}, $proxy;
+  });
+}
+
+sub use_proxy {
+  my $self = shift;
+  my $proxy = shift @{$self->list}
+    || ($self->proxy_load && shift @{$self->list});
+  #~ $self->render(json=>$r);
+  #~ $self->check_proxy($r);
+  return 'socks://' . $proxy;
+}
+
+sub check_proxy {
+  my ($self, $proxy) = @_;
+  my $save_proxy = $self->ua->proxy->https || $self->ua->proxy->http;
+  #~ my $schema = lc((split /,/, $proxy->{type})[-1]);
+  $self->ua->proxy->https($proxy)->http($proxy);
+  my $res = $self->ua->get($self->check_url. (rand =~ /(\d{3,7})/)[0],);
+  $self->ua->proxy->https($save_proxy)->http($save_proxy);
+  #~ die sprintf("Ошибка запроса [%s] проверки прокси: %s", $self->check_url, $res)
+  #~ $self->model->status_proxy(ref $res ? 'G' : 'B', $proxy);
+  ref $res ? $self->good_proxy($proxy)
+    : $self->bad_proxy($proxy);
+  #~ return $self->dumper();
+    #~ unless ref $res;
+  #~ $res->code;
+}
+
+sub good_proxy {
+  my ($self, $proxy) = @_;
+  $proxy = ( $proxy =~ /([\d\.]+:\d+)$/ )[0]
+    or return;
+  #~ $self->model->status_proxy('G', $proxy);
+}
+
+sub bad_proxy {
+  my ($self, $proxy) = @_;
+  $proxy = ( $proxy =~ /([\d\.]+:\d+)$/ )[0]
+    or return;
+  #~ $self->model->status_proxy('B', $proxy);
+}
+
+
+
+1;
